08631bf70fa4da1f45cfbc1c11779211abd3213b
Vir <73329256+Vir-8@users.noreply.github.com>
2023-06-07 20:14:03 +0530

    Add UI and stylize

--- script.js
+++ script.js
@@ -1,41 +1,75 @@
 let playerPoints = 0;
 let pcPoints = 0;
+let computerSelection;
+
+
+const rock = document.querySelector('#one');
+const paper = document.querySelector('#two');
+const scissors = document.querySelector('#three');
+
+const res = document.querySelector('.result');
+const scor = document.querySelector('.score');
+
+const restart = document.querySelector('.restart');
+
+const reButton = document.createElement('button');
+reButton.textContent = "Wanna play again?"
+reButton.setAttribute('class', 'restart');
+
+// rock.onclick = playRound('rock', computerSelection); <= doesn't work expectedly, executes on reload and does nothing on clicking
+
+rock.onclick = function() {
+    playRound('rock', computerSelection);
+};
+
+paper.onclick = function() {
+    playRound('paper', computerSelection);
+};
+
+scissors.onclick = function() {
+    playRound('scissors', computerSelection);
+};
+
+
+
 
 function playRound(playerSelection, computerSelection) 
 {
+    computerSelection = getComputerChoice();
+
     
     if (playerSelection === computerSelection)
     {
-        console.log("Tie!");
+        res.textContent = "Tie!";
     }
     else if (playerSelection === "rock" && computerSelection === "paper")
     {
-        console.log("PC wins, paper beats rock!");
+        res.textContent = "PC wins, paper beats rock!";
         pcPoints++;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors")
     {
-        console.log("Player wins, rock beats scissors!");
+        res.textContent = "Player wins, rock beats scissors!";
         playerPoints++;
     }
     else if (playerSelection === "paper" && computerSelection === "rock")
     {
-        console.log("Player wins, paper beats rock!");
+        res.textContent = "Player wins, paper beats rock!";
         playerPoints++;
     }
     else if (playerSelection === "paper" && computerSelection === "scissors")
     {
-        console.log("PC wins, scissors cut paper!");
+        res.textContent = "PC wins, scissors cut paper!";
         pcPoints++;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock")
     {
-        console.log("PC wins, rock beat scissors!");
+        res.textContent = "PC wins, rock beat scissors!";
         pcPoints++;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper")
     {
-        console.log("Player wins, scissors cut paper!");
+        res.textContent = "Player wins, scissors cut paper!";
         playerPoints++;
     }
     else
@@ -43,36 +77,48 @@ function playRound(playerSelection, computerSelection)
         console.log("error");
     }
 
+    scor.textContent = playerPoints + " - " + pcPoints;
+
+
+    if (playerPoints === 5) 
+    {
+        res.textContent = "Player wins!";
+        disableButtons();
+        restartButton();
+    } 
+    else if (pcPoints === 5) 
+    {
+        res.textContent = "Computer wins!";
+        disableButtons();
+        restartButton();
+    }
+
 }
   
-
-// console.log(playRound(playerSelection, computerSelection));
-
-function game()
+function disableButtons()
 {
-    while (playerPoints !== 5 && pcPoints !== 5)
-    {
-        let playerSelection = prompt("Rock, paper scissors?");
-        playerSelection = playerSelection.toLowerCase();
-        let computerSelection = getComputerChoice();
-
-        playRound(playerSelection, computerSelection);
-
-        if (playerPoints === 5 || pcPoints === 5) {
-            break;
-        }
-    }
-
-    if(playerPoints === 5)
-    {
-        console.log("Player wins!! Score is " + playerPoints + "-" + pcPoints);
-    }
-
-    if(pcPoints === 5)
-    {
-        console.log("Player loses. Score is " + playerPoints + "-" + pcPoints);
-    }
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
 }
+
+function restartButton()
+{
+    console.log("Restart game?");
+    restart.append(reButton);
+}
+
+reButton.onclick = function() {
+    playerPoints = pcPoints = 0;
+    console.log("restarting");
+    scor.textContent = playerPoints + " - " + pcPoints;
+
+    rock.disabled = false;
+    paper.disabled = false;
+    scissors.disabled = false;
+
+    reButton.remove();
+};
 
 
 function getComputerChoice()
@@ -96,10 +142,3 @@ function getComputerChoice()
         return "error";
     }
 }
-
-
-
-
-
-
-game();
